9ac89502f998a1de00098c19e0789bbe9b7b7290
JakeAsh22 <jacob.ash1998@gmail.com>
2020-01-31 11:01:31 -0800

    Create Animal shelter context

--- Models/AnimalShelterContext.cs
+++ Models/AnimalShelterContext.cs
@@ -1,11 +1,25 @@
-namespace CretaceousPark.Models
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelterAPI.Solution.Models
 {
-    public class Animal
+    public class AnimalShelterContext : DbContext
     {
-        public int AnimalId { get; set; }
-        public string Name { get; set; }
-        public string Species { get; set; }
-        public int Age { get; set; }
-        public string Gender { get; set; }
+        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Animal>()
+                .HasData(
+                    new Animal { AnimalId = 1, Name = "Matilda", Species = "Cat", Age = 7, Gender = "Female", Description = "Freindly cat that loves getting pet" },
+                    new Animal { AnimalId = 2, Name = "Rexie", Species = "Cat", Age = 10, Gender = "Female", Description = "A cat with a personaility, and she will let you know how she is feeling" },
+                    new Animal { AnimalId = 3, Name = "Matilda", Species = "Dog", Age = 2, Gender = "Female", Description = "Friendly dog that lives to cuddle and hang out with people" },
+                    new Animal { AnimalId = 4, Name = "Pip", Species = "Dog", Age = 4, Gender = "Male", Description = "A ball of energy who loves to play anytime possible" },
+                    new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Dog", Age = 12, Gender = "Male", Description = "Was found on the streets, shy at first but once he warms up with you he'll never leave your side" }
+                );
+        }
     }
 }
