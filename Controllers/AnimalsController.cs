ba006c1589a8ce61ce7214401a8650e901b6d142
JakeAsh22 <jacob.ash1998@gmail.com>
2020-01-31 16:30:03 -0800

    Update Animal Controller

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -4,44 +4,71 @@ using Microsoft.AspNetCore.Mvc;
 using AnimalShelterAPI.Solution.Models;
 using Microsoft.EntityFrameworkCore;
 
-
-
-namespace AnimalShelterAPI.Solution.Controllers
+namespace CretaceousPark.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class AnimalsController : ControllerBase
     {
-        // GET api/values
+        private AnimalShelterContext _db;
+
+        public AnimalsController(AnimalShelterContext db)
+        {
+            _db = db;
+        }
+
+        // GET api/animals
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Animal>> Get(string species, string gender, string name)
         {
-            return new string[] { "value1", "value2" };
+            var query = _db.Animals.AsQueryable();
+
+            if (species != null)
+            {
+                query = query.Where(entry => entry.Species == species);
+            }
+
+            if (gender != null)
+            {
+                query = query.Where(entry => entry.Gender == gender);
+            }
+
+            if (name != null)
+            {
+                query = query.Where(entry => entry.Name == name);
+            }
+
+            return query.ToList();
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
+        // POST api/animals
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Animal animal)
         {
+            _db.Animals.Add(animal);
+            _db.SaveChanges();
         }
-
-        // PUT api/values/5
+        // GET api/animals/5
+        [HttpGet("{id}")]
+        public ActionResult<Animal> Get(int id)
+        {
+            return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+        }
+        // PUT api/animals/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Animal animal)
         {
+            animal.AnimalId = id;
+            _db.Entry(animal).State = EntityState.Modified;
+            _db.SaveChanges();
         }
-
-        // DELETE api/values/5
+        // DELETE api/animals/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var animalToDelete = _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+            _db.Animals.Remove(animalToDelete);
+            _db.SaveChanges();
         }
     }
 }
